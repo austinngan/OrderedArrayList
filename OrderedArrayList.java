4d6c2b7798dd04b34a025816641603524f51820f
austinngan <angan20@stuy.edu>
2020-12-11 18:36:54 -0500

    added findIndex helper and first add method

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.lang.IllegalArgumentException;
 
 public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T> {
 	public OrderedArrayList() {
@@ -6,5 +7,21 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
 	}
 	public OrderedArrayList(int start){
 		super(start);
+	}
+	private int findIndex(T value){
+		if (value==null){
+			throw new IllegalArgumentException("Element cannot be null");
+		}
+		for (int i=0;i<size()-1;i++){
+			if (value.compareTo(get(i))>=0&&value.compareTo(get(i+1))<=0){
+				return i+1;
+			}
+		}
+		return size();
+	}
+	public boolean add(T value){
+		int index=findIndex(value);
+		super.add(index,value);
+		return true;
 	}
 }
