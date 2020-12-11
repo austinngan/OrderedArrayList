f00ba9851fe936df231744f6a3e225d0bb71d524
austinngan <angan20@stuy.edu>
2020-12-10 21:08:36 -0500

    Added constructors for NoNullArrayList

---
+++ NoNullArrayList.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.lang.IllegalArgumentException;
+
+public class NoNullArrayList<T> extends ArrayList<T>{
+  public NoNullArrayList(){
+    super();
+  }
+  public NoNullArrayList(int start){
+    super(start);
+  }
+}
